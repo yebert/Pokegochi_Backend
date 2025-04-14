f8e9d7bbe36bf706f620c7cac0bda0bd76ca4e80
Yannis Ebert <yannis.ebert@web.de>
2025-04-15 00:39:20 +0200

    URI deleted

--- db/index.js
+++ db/index.js
@@ -1,8 +1,5 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(process.env.PG_URI);
-// const sequelize = new Sequelize(
-//   "postgresql://pokegochi_db_owner:npg_J2NiExtzvK8f@ep-dark-hill-a2ucof1r-pooler.eu-central-1.aws.neon.tech/pokegochi_db?sslmode=require"
-// );
 
 export default sequelize;
